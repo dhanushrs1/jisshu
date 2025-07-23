539116409d7b69e5eea6e2db791c32ae1bc2cc71
Dhanush R.S <Dhanushrs013@gmail.com>
2025-07-23 15:34:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,7 +19,7 @@ SESSION = environ.get("SESSION", "Media_search")
 API_ID = int(environ.get("API_ID", ""))
 API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-PORT = environ.get("PORT", "8082")
+PORT = environ.get("PORT", "")
 
 # Owners
 ADMINS = [
